94878b00b144b8d359e53cadbce2fbdae39345e9
Sean Haile <seanhaile.codes@gmail.com>
2019-06-26 15:50:38 -0400

    next and back slide is working just need to adjust styling

--- scripts/app.js
+++ scripts/app.js
@@ -13,11 +13,33 @@ const setSlidePosition = (slide, index) => {
 }
 slides.forEach(setSlidePosition)
 
+const moveToSlide = (track, currentSlide, targetSlide) => {
+    track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
+    currentSlide.classList.remove('current-slide');
+    targetSlide.classList.add('current-slide');
+}
+
+prevButton.addEventListener('click', e => {
+    const currentSlide = track.querySelector('.current-slide');
+    const prevSlide = currentSlide.previousElementSibling;
+
+    moveToSlide(track, currentSlide, prevSlide)
+})
+
 nextButton.addEventListener('click', e => {
     const currentSlide = track.querySelector('.current-slide');
     const nextSlide = currentSlide.nextElementSibling;
-    const amountToMove = nextSlide.style.left;
-    track.style.transform = 'translateX(-' + amountToMove + ')';
-    currentSlide.classList.remove('current-slide');
-    nextSlide.classList.add('current-slide')
+
+    moveToSlide(track, currentSlide, nextSlide);
 })
+
+//code prevButton!!
+
+// prevButton.addEventListener('click', e => {
+//     const currentSlide = track.querySelector('.current-slide');
+//     const prevSlide = currentSlide.prevElementSibling;
+//     const amountToMove = prevtSlide.style.left;
+//     track.style.transform = 'translateX(-' + amountToMove + ')';
+//     currentSlide.classList.remove('current-slide');
+//     prevSlide.classList.add('current-slide')
+// })
